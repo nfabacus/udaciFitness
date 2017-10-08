e3d3f6751cda0be47fa7949a020afe88452bc353
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-10-08 16:07:54 +0100

    adds some icons

---
+++ components/UdaciSteppers.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function udaciSteppers () {
+  return (
+    <View>
+      <Text>UdaciSteppers</Text>
+    </View>
+  )
+}

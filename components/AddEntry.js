1b6734aef49d89288924cc6e7547a10395221033
noby <n.fujioka@abacusmaths.info>
2017-10-14 21:23:55 +0100

    adds some styling.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, ScrollView } from 'react-native'
+import { View, TouchableOpacity, Text, ScrollView, Platform, StyleSheet } from 'react-native'
 import { getMetricMetaInfo, timeToString, getDailyReminderValue } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
@@ -10,14 +10,16 @@ import { submitEntry, removeEntry } from '../utils/api'
 
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { white, purple, red, orange, blue, lightPurp, pink, gray } from '../utils/colors'
 
 
 const SubmitBtn = ({ onPress })=>{
   return (
     <TouchableOpacity
+      style={Platform.OS==='ios'? styles.iosSubmitBtn: styles.androidSubmitBtn}
       onPress={ onPress }
     >
-      <Text>Submit</Text>
+      <Text style={styles.submitBtntext}>Submit</Text>
     </TouchableOpacity>
   )
 }
@@ -104,13 +106,16 @@ class AddEntry extends Component {
 
     if(this.props.alreadyLogged) {
       return (
-        <View>
+        <View style={ styles.center}>
           <Ionicons
-            name="ios-happy-outline"
+            name={Platform.OS === 'ios'? 'ios-happy-outline':'md-happy'}
             size={100}
           />
           <Text>You already logged your information for Today.</Text>
-          <TextButton onPress={this.reset}>
+          <TextButton
+            style={ {padding: 10 } }
+            onPress={this.reset}
+          >
             Reset
           </TextButton>
         </View>
@@ -118,7 +123,7 @@ class AddEntry extends Component {
     }
 
     return (
-      <ScrollView>
+      <ScrollView style={ styles.container }>
         <DateHeader date={(new Date()).toLocaleDateString()}/>
         {
           Object.keys(metaInfo).map((key)=>{
@@ -126,7 +131,7 @@ class AddEntry extends Component {
             const value = this.state[key]
 
             return (
-              <View key={key}>
+              <View key={key} style={styles.row}>
                 {getIcon()}
                 {type === "slider"
                   ? <UdaciSlider
@@ -151,6 +156,51 @@ class AddEntry extends Component {
 
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    margin: 5
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submitBtntext: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 30,
+    marginLeft: 30,
+  }
+})
 
 function mapStateToProps(state) {
   const key= timeToString()
--- components/TextButton.js
+++ components/TextButton.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { white, purple, gray } from '../utils/colors'
 
-export default function TextButton ({ children, onPress }) {
+export default function TextButton ({ children, onPress, style = {} }) {
   return (
     <TouchableOpacity onPress={onPress}>
-      <Text>{children}</Text>
+      <Text style={ [styles.reset, style] }>{children}</Text>
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: 'center',
+    color: white,
+    backgroundColor: purple,
+    borderRadius: 5,
+    marginTop: 5
+    
+  }
+})
